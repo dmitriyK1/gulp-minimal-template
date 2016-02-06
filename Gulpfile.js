cfd812c1147dab17ec10e949e8a9aa7561c6bf1d
Dmitriy <email@gmail.com>
2016-02-06 17:16:48 +0200

    add gulp-load-plugins

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,38 +8,12 @@
 //
 // https://github.com/postcss/postcss-safe-parser
 
-const isProduction      = require('gulp-util').env.p
 const gulp              = require('gulp')
-const sass              = require('gulp-sass')
-const babel             = require('gulp-babel')
-const coffee            = require('gulp-coffee')
-const jade              = require('gulp-jade')
-const stylus            = require('gulp-stylus')
-const jshint            = require('gulp-jshint')
-const coffeelint        = require('gulp-coffeelint')
-const rev               = require('gulp-rev-append')
-const autoprefixer      = require('gulp-autoprefixer')
-const plumber           = require('gulp-plumber')
-const changed           = require('gulp-changed')
-const sourcemaps        = require('gulp-sourcemaps')
+const plugins           = require('gulp-load-plugins')()
+const isProduction      = plugins.util.env.p
 const runSequence       = require('run-sequence')
-const clean             = require('gulp-clean')
-const csscomb           = require('gulp-csscomb')
-const jscs              = require('gulp-jscs')
 const browserSync       = require('browser-sync')
-const stylish           = require('gulp-jscs-stylish')
-const uglify            = require('gulp-uglify')
-const minify            = require('gulp-htmlmin')
-const minifyInline      = require('gulp-minify-inline')
-const prettify          = require('gulp-prettify')
-const mmq               = require('gulp-merge-media-queries')
-const csso              = require('gulp-csso')
-const beautify          = require('gulp-jsbeautifier')
-const stripDebug        = require('gulp-strip-debug')
 const poststylus        = require('poststylus')
-const cssnano           = require('gulp-cssnano')
-const ngAnnotate        = require('gulp-ng-annotate')
-const posthtml          = require('gulp-posthtml')
 const posthtmlBemConfig = {
         elemPrefix: '__',
         modPrefix: '_',
@@ -49,10 +23,11 @@ const posthtmlPlugins = [
       require('posthtml-lorem')()
     , require('posthtml-bem')( posthtmlBemConfig )
     , require('posthtml-img-autosize')({
-          root: './' // Path to images base directory (default: './')
+          root: './'
         , processEmptySize: true
     })
 ]
+
 // const postcss         = require('gulp-postcss')
 // const postcssInitial  = require('postcss-initial')
 // const postcssPosition = require('postcss-position')
@@ -64,93 +39,92 @@ const posthtmlPlugins = [
 gulp.task( 'sass', () =>
     gulp
         .src('./dev/sass/**/*.sass')
-        .pipe( plumber( log ) )
-        .pipe( sourcemaps.init() )
-        .pipe( sass({ outputStyle: isProduction ? 'compressed' : undefined }).on( 'error', sass.logError ) )
-        .pipe( sourcemaps.write('./dist/css') )
+        .pipe( plugins.plumber( log ) )
+        .pipe( plugins.sourcemaps.init() )
+        .pipe( plugins.sass({ outputStyle: isProduction ? 'compressed' : undefined }).on( 'error', plugins.sass.logError ) )
+        .pipe( plugins.sourcemaps.write('./dist/css') )
         .pipe( gulp.dest('./dist/css') )
 )
 
 gulp.task( 'coffee', () =>
     gulp
         .src('./dev/coffee/**/*.coffee')
-        .pipe( plumber( log ) )
-        .pipe( changed( './dist/js', { extension: '.js' } ) )
-        .pipe( coffeelint() )
-        .pipe( coffeelint.reporter() )
-        .pipe( sourcemaps.init() )
-        .pipe( coffee({ bare: true }) )
-        .pipe( ngAnnotate() )
-        .pipe( beautify({ config: '.jsbeautifyrc' }) )
-        .pipe( jscs({ fix: true }) )
-        .pipe( jshint() )
-        .pipe( jshint.reporter() )
-        .pipe( stylish.combineWithHintResults() )
-        .pipe( sourcemaps.write('.') )
+        .pipe( plugins.plumber( log ) )
+        .pipe( plugins.changed( './dist/js', { extension: '.js' } ) )
+        .pipe( plugins.coffeelint() )
+        .pipe( plugins.coffeelint.reporter() )
+        .pipe( plugins.sourcemaps.init() )
+        .pipe( plugins.coffee({ bare: true }) )
+        .pipe( plugins.ngAnnotate() )
+        .pipe( plugins.jsbeautifier({ config: '.jsbeautifyrc' }) )
+        .pipe( plugins.jscs({ fix: true }) )
+        .pipe( plugins.jshint() )
+        .pipe( plugins.jshint.reporter() )
+        .pipe( plugins.jscsStylish.combineWithHintResults() )
+        .pipe( plugins.sourcemaps.write('.') )
         .pipe( gulp.dest('./dist/js') )
 )
 
 gulp.task( 'jsx', () =>
     gulp
         .src('./dev/jsx/**/*.jsx')
-        .pipe( plumber( log ) )
-        .pipe( changed( './dist/js', { extension: '.js' } ) )
-        .pipe( babel() )
-        .pipe( ngAnnotate() )
-        .pipe( beautify({ config: '.jsbeautifyrc' }) )
-        .pipe( jscs({ fix: true }) )
-        .pipe( jshint() )
-        .pipe( jshint.reporter() )
-        .pipe( stylish.combineWithHintResults() )
+        .pipe( plugins.plumber( log ) )
+        .pipe( plugins.changed( './dist/js', { extension: '.js' } ) )
+        .pipe( plugins.babel() )
+        .pipe( plugins.ngAnnotate() )
+        .pipe( plugins.jsbeautifier({ config: '.jsbeautifyrc' }) )
+        .pipe( plugins.jscs({ fix: true }) )
+        .pipe( plugins.jshint() )
+        .pipe( plugins.jshint.reporter() )
+        .pipe( plugins.jscsStylish.combineWithHintResults() )
         .pipe( gulp.dest('./dist/js') )
 )
 
 gulp.task( 'es6', () =>
     gulp
         .src('./dev/es6/**/*.js')
-        .pipe( plumber( log ) )
-        .pipe( changed( './dist/js', { extension: '.js' } ) )
-        .pipe( babel() )
-        .pipe( ngAnnotate() )
-        .pipe( beautify({ config: '.jsbeautifyrc' }) )
-        .pipe( jscs({ fix: true }) )
-        .pipe( jshint() )
-        .pipe( jshint.reporter() )
-        .pipe( stylish.combineWithHintResults() )
+        .pipe( plugins.plumber( log ) )
+        .pipe( plugins.changed( './dist/js', { extension: '.js' } ) )
+        .pipe( plugins.babel() )
+        .pipe( plugins.ngAnnotate() )
+        .pipe( plugins.jsbeautifier({ config: '.jsbeautifyrc' }) )
+        .pipe( plugins.jscs({ fix: true }) )
+        .pipe( plugins.jshint() )
+        .pipe( plugins.jshint.reporter() )
+        .pipe( plugins.jscsStylish.combineWithHintResults() )
         .pipe( gulp.dest('./dist/js') )
 )
 
 gulp.task( 'js', () =>
     gulp
         .src('./dev/js/**/*.js')
-        .pipe( plumber( log ) )
-        .pipe( changed( './dist/js', { extension: '.js' } ) )
-        .pipe( ngAnnotate() )
-        .pipe( beautify({ config: '.jsbeautifyrc' }) )
-        .pipe( jscs({ fix: true }) )
-        .pipe( jshint() )
-        .pipe( jshint.reporter() )
-        .pipe( stylish.combineWithHintResults() )
+        .pipe( plugins.plumber( log ) )
+        .pipe( plugins.changed( './dist/js', { extension: '.js' } ) )
+        .pipe( plugins.ngAnnotate() )
+        .pipe( plugins.jsbeautifier({ config: '.jsbeautifyrc' }) )
+        .pipe( plugins.jscs({ fix: true }) )
+        .pipe( plugins.jshint() )
+        .pipe( plugins.jshint.reporter() )
+        .pipe( plugins.jscsStylish.combineWithHintResults() )
         .pipe( gulp.dest('./dist/js') )
 )
 
 gulp.task( 'jade', () =>
     gulp
         .src('./dev/jade/**/*.jade')
-        .pipe( plumber( log ) )
-        .pipe( changed( './dist', { extension: '.html' } ) )
-        .pipe( jade({ pretty: true }) )
-        .pipe( posthtml( posthtmlPlugins ) )
-        .pipe( rev() )
+        .pipe( plugins.plumber( log ) )
+        .pipe( plugins.changed( './dist', { extension: '.html' } ) )
+        .pipe( plugins.jade({ pretty: true }) )
+        .pipe( plugins.posthtml( posthtmlPlugins ) )
         .pipe(
-            prettify({
-                  brace_style: 'expand'
-                , indent_size: 1
-                , indent_char: '\t'
-                , indent_with_tabs: true
-                , condense: true
-                , indent_inner_html: true
-                , preserve_newlines: true
+            plugins.prettify({
+                  brace_style       : 'expand'
+                , indent_size       : 1
+                , indent_char       : '\t'
+                , indent_with_tabs  : true
+                , condense          : true
+                , indent_inner_html : true
+                , preserve_newlines : true
             })
         )
         .pipe( gulp.dest('./dist') )
@@ -159,30 +133,30 @@ gulp.task( 'jade', () =>
 gulp.task('rev', () =>
     gulp
     .src('./dist/*.html')
-    .pipe( rev() )
+    .pipe( plugins.revAppend() )
     .pipe( gulp.dest('./dist/') )
 )
 
 gulp.task( 'stylus', () =>
     gulp
         .src('./dev/stylus/**/*.styl')
-        .pipe( plumber( log ) )
-        .pipe( changed( './dist/css', { extension: '.css' } ) )
-        .pipe( sourcemaps.init() )
-        .pipe( stylus({
+        .pipe( plugins.plumber( log ) )
+        .pipe( plugins.changed( './dist/css', { extension: '.css' } ) )
+        .pipe( plugins.sourcemaps.init() )
+        .pipe( plugins.stylus({
             use: [
                 // poststylus([ 'rucksack-css', 'postcss-autoreset', 'postcss-initial', 'postcss-position', 'postcss-normalize', 'postcss-cssnext' ])
                 poststylus([ 'rucksack-css', 'postcss-position', 'postcss-normalize', 'postcss-cssnext', 'postcss-remove-prefixes', 'postcss-flexboxfixer', 'postcss-gradientfixer' ])
             ]
         }))
         // .pipe( postcss(postcssPlugins) )
-        .pipe( csscomb() )
-        .pipe( autoprefixer({ browsers: ['last 2 versions'] }) )
-        .pipe( sourcemaps.write('.') )
+        .pipe( plugins.csscomb() )
+        .pipe( plugins.autoprefixer({ browsers: ['last 2 versions'] }) )
+        .pipe( plugins.sourcemaps.write('.') )
         .pipe( gulp.dest('./dist/css') )
 )
 
-gulp.task( 'clean', () => gulp.src('./dist').pipe( clean() ) )
+gulp.task( 'clean', () => gulp.src('./dist').pipe( plugins.clean() ) )
 
 gulp.task('browsersync', () =>
     browserSync.init(
@@ -201,24 +175,27 @@ gulp.task( 'compress', () => {
 
     gulp
         .src('dist/js/*.js')
-        .pipe( plumber( log ) )
-        .pipe( stripDebug() )
-        .pipe( uglify() )
+        .pipe( plugins.plumber( log ) )
+        .pipe( plugins.stripDebug() )
+        .pipe( plugins.uglify({ mangle: true }) )
         .pipe( gulp.dest('dist/js') )
 
     gulp
         .src('dist/*.html')
-        .pipe( plumber( log ) )
-        .pipe( minify({ collapseWhitespace: true }) )
-        .pipe( minifyInline() )
+        .pipe( plugins.plumber( log ) )
+        .pipe( plugins.htmlmin({ collapseWhitespace: true }) )
+        .pipe( plugins.minifyInline() )
         .pipe( gulp.dest('dist') )
 
     gulp
         .src('dist/css/*.css')
-        .pipe( plumber( log ) )
-        .pipe( mmq() )
-        .pipe( csso() )
-        .pipe( cssnano() )
+        .pipe( plugins.plumber( log ) )
+        .pipe( plugins.mergeMediaQueries() )
+        .pipe( plugins.csso() )
+        .pipe( plugins.cssnano({
+                discardComments: { removeAll: true }
+            })
+        )
         .pipe( gulp.dest('dist/css') )
 })
 
