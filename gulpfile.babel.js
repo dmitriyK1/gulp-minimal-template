3051d6da159bb81553e586a459c0e9fed8aa78cd
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2016-06-07 15:37:29 +0300

    up

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -190,12 +190,12 @@ gulp.task( 'compress', () => {
 })
 
 gulp.task( 'critical', () => critical.generate({
-      inline : true,
-      base   : 'dist/',
-      src    : 'index.html',
-      dest   : 'dist/index-critical.html',
-      width  : 1300,
-      height : 900
+        inline : true
+      , base   : 'dist/'
+      , src    : 'index.html'
+      , dest   : 'dist/index-critical.html'
+      , width  : 1300
+        height : 900
   })
 )
 
